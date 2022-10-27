486ef0236ee9499fe25a98a1fc6c64dea309541c
kook <whdrnrdldia@naver.com>
2022-10-28 00:25:46 +0900

    JS_Handle Score Event

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // Global variables
 const CARROT_COUNT = 10;
 const CARROT_SIZE = 80;
@@ -24,12 +26,10 @@ const resultMsg = {
   stop: "MORE TRY❓",
 };
 
+// handle Btn
 gameBtn.addEventListener("click", () => {
   if (started) stopGame();
-  else {
-    startGame();
-  }
-  started = !started;
+  else startGame();
 });
 
 popupBtn.addEventListener("click", () => {
@@ -38,34 +38,40 @@ popupBtn.addEventListener("click", () => {
 });
 
 function startGame() {
+  started = true;
   initGame();
   startTimer();
 }
 function stopGame() {
+  started = false;
   stopTimer();
   showPopupMsg(resultMsg.stop);
 }
-function finishGame() {
-  showPopupMsg(resultMsg.win ? resultMsg.win : resultMsg.lose);
+function finishGame(win) {
+  started = false;
+  stopTimer();
+  showPopupMsg(win ? resultMsg.win : resultMsg.lose);
 }
 
 function initGame() {
+  //   score = 0;
+  //   gameField.innerHTML = "";
+  gameScore.innerText = CARROT_COUNT;
   addItem("carrot", CARROT_COUNT, "/img/carrot.png");
   addItem("bug", CARROT_COUNT, "/img/bug.png");
 }
 
 function addItem(className, count, imgPath) {
   for (let i = 0; i < count; i++) {
-    // Random coordinates
     const x1 = 0;
     const y1 = 0;
     const x2 = fieldRect.width - CARROT_SIZE;
     const y2 = fieldRect.height - CARROT_SIZE;
     const x = randomCoordinate(x1, x2);
     const y = randomCoordinate(y1, y2);
-    // create item
+
     const item = document.createElement("img");
-    item.setAttribute("className", className);
+    item.setAttribute("class", className);
     item.setAttribute("src", imgPath);
     item.style.position = "absolute";
     item.style.left = `${x}px`;
@@ -80,12 +86,12 @@ function randomCoordinate(min, max) {
 
 // handle pop-up
 function showPopupMsg(text) {
-  popup.style.display = "block";
+  popup.classList.remove("pop-up--hide");
   popupText.innerHTML = text;
 }
 
 function hidePopupMsg() {
-  popup.style.visibility = "hidden";
+  popup.classList.add("pop-up--hide");
 }
 
 // handle timer
@@ -95,6 +101,7 @@ function startTimer() {
   timer = setInterval(() => {
     if (remainingTimeSec <= 0) {
       clearInterval(timer);
+      finishGame(CARROT_COUNT === score);
       showPopupMsg(resultMsg.lose);
       return;
     }
@@ -102,12 +109,37 @@ function startTimer() {
   }, 1000);
 }
 
-function stopTimer() {
-  clearInterval(timer);
-}
-
 function updateTimer(time) {
   let minutes = Math.floor(time / 60);
   let seconds = time % 60;
-  gameTimer.innerText = `${minutes} : ${seconds}`;
+  gameTimer.innerHTML = `${minutes} : ${seconds}`;
+}
+
+function stopTimer() {
+  clearInterval(timer);
+}
+// handle score
+gameField.addEventListener("click", (e) => {
+  onFieldScore(e);
+});
+
+function onFieldScore(e) {
+  if (!started) {
+    return;
+  }
+  const target = e.target;
+  if (target.matches(".carrot")) {
+    target.remove();
+    score++;
+    updateScore();
+    if (score === CARROT_COUNT) {
+      finishGame(true);
+    }
+  } else if (target.matches(".bug")) {
+    finishGame(false);
+  }
+}
+
+function updateScore() {
+  gameScore.innerText = CARROT_COUNT - score;
 }
